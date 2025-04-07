1cd74ce1db990e3d81bb20d7430087823a7641fc
Aashutosh <aashutoshdahal22@gmail.com>
2025-04-08 02:35:37 +0545

    flow improvement bug fixes history api not started

--- controllers/Meds/autoCompleteController.js
+++ controllers/Meds/autoCompleteController.js
@@ -18,3 +18,24 @@ module.exports.medicines = async(req,res)=>{
         errorResponse(res,500,"Internal Server Error")
     }
 }
+
+module.exports.diseases = async (req, res) => {
+  const { query } = req.query;
+
+  if (!query || query.length < 3) {
+    return res.json([]);
+  }
+
+  try {
+    const apiURL = `${process.env.API_disease}&terms=${query}`;
+    const response = await fetch(apiURL);
+    const data = await response.json();
+
+    const diseases = data[3] || [];
+    const result = diseases.map(d => d[0]); // d is an array like ["Bone cancer"]
+
+    res.json(result);
+  } catch (error) {
+    errorResponse(res, 500, "Internal Server Error");
+  }
+};
